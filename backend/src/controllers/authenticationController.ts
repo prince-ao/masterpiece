01050e6b8237c3aced571eb07e823ecfcb7b7fbc
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-08-22 09:33:01 -0400

    signup route done

---
+++ backend/src/controllers/authenticationController.ts
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import express from "express";
+import pool from "../db";
+import { Validate } from "../middleware";
+import client from "../cache";
+
+const router = express.Router();
+
+router.get("/signup", Validate.validateSignup, async (req, res) => {
+    try {
+        let users = await pool.query(
+            "SELECT user_id FROM m_user WHERE username = $1 OR email = $2",
+            [req.body.username, req.body.email]
+        );
+
+        if (users.rows.length > 0) {
+            return res.status(400).send({
+                error_message: "user already exists",
+            });
+        }
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(req.body.password, salt);
+
+        await pool.query(
+            "INSERT INTO m_user(username, email, password, created_at) values ($1, $2, $3, $4)",
+            [req.body.username, req.body.email, hash, Date.now()]
+        );
+        users = await pool.query(
+            "SELECT user_id FROM m_user WHERE username = $1",
+            [req.body.username]
+        );
+
+        const hour = 3600;
+        const day = 24 * hour;
+
+        const token = jwt.sign(
+            { user_id: users.rows[0].user_id },
+            process.env.JWT_TOKEN as string,
+            { expiresIn: day }
+        );
+
+        await client.connect();
+
+        client.set(token, 1, { EXAT: day });
+
+        await client.disconnect();
+
+        return res.status(200).send({
+            token,
+        });
+    } catch (err) {
+        res.status(400).json({ error_message: "Unknown error" });
+    }
+});
+
+export default router;
