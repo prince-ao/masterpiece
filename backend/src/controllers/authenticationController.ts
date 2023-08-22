b37f5e2051c3ce4e7da26cae26be274cfdc7d44b
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-08-22 11:20:37 -0400

    login route done

--- backend/src/controllers/authenticationController.ts
+++ backend/src/controllers/authenticationController.ts
@@ -6,6 +6,9 @@ import { Validate } from "../middleware";
 import client from "../cache";
 
 const router = express.Router();
+const saltRounds = 10;
+const hour = 3600;
+const day = 24 * hour;
 
 router.post("/signup", Validate.validateSignup, async (req, res) => {
     try {
@@ -20,7 +23,6 @@ router.post("/signup", Validate.validateSignup, async (req, res) => {
             });
         }
 
-        const saltRounds = 10;
         const salt = await bcrypt.genSalt(saltRounds);
         const hash = await bcrypt.hash(req.body.password, salt);
 
@@ -32,9 +34,6 @@ router.post("/signup", Validate.validateSignup, async (req, res) => {
             "SELECT user_id FROM m_user WHERE username = $1",
             [req.body.username]
         );
-
-        const hour = 3600;
-        const day = 24 * hour;
 
         const token = jwt.sign(
             { user_id: users.rows[0].user_id },
@@ -55,6 +54,35 @@ router.post("/signup", Validate.validateSignup, async (req, res) => {
         console.log(err);
         res.status(400).json({ error_message: "Unknown error" });
     }
+});
+
+router.post("/login", Validate.validateLogin, async (req, res) => {
+    let users = await pool.query(
+        "SELECT user_id, password FROM m_user WHERE username = $1 OR email = $2",
+        [req.body.username, req.body.email]
+    );
+
+    if (users.rows.length === 0)
+        return res.status(400).send({ error_message: "User not found" });
+
+    if (!(await bcrypt.compare(req.body.password, users.rows[0].password)))
+        return res.status(400).send({ error_message: "Invalid password" });
+
+    const token = jwt.sign(
+        { user_id: users.rows[0].user_id },
+        process.env.JWT_SECRET as string,
+        { expiresIn: day }
+    );
+
+    await client.connect();
+
+    await client.set(token, 1, { EXAT: day });
+
+    await client.disconnect();
+
+    return res.status(200).send({
+        token,
+    });
 });
 
 export default router;
--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -3,6 +3,30 @@ import { isLeft } from "fp-ts/Either";
 import { Request, Response, NextFunction } from "express";
 
 export default class Validate {
+    static validateLogin(req: Request, res: Response, next: NextFunction) {
+        const Login = t.type({
+            username: t.union([t.string, t.undefined]),
+            email: t.union([t.string, t.undefined]),
+            password: t.string,
+        });
+
+        const decoded = Login.decode(req.body);
+
+        if (isLeft(decoded)) {
+            return res.status(400).send({
+                error_message: "Invalid body",
+            });
+        }
+
+        if (req.body.username === undefined && req.body.email == undefined) {
+            return res.status(400).send({
+                error_message: "Invalid body",
+            });
+        }
+
+        next();
+    }
+
     static validateSignup(req: Request, res: Response, next: NextFunction) {
         const Signup = t.type({
             username: t.string,
