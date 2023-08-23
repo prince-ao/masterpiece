c3e7922df1f053e1f1401e3bdcc1dcf2bee18835
AlanM03 <alanmackiewicz@yahoo.com>
2023-08-22 21:47:20 -0400

    added painting like feature

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -7,16 +7,30 @@ import authenticateToken from "../middleware/authenticateToken";
 
 const router = express.Router();
 
-router.get("/", authenticateToken, async (req, res) => {
+router.post("/:painting_id/like", authenticateToken, async (req, res) => {
     try {
-        const authenticatedUser = (req as any).user_id;//how to access the decoded info
-        //const users = await pool.query("SELECT * FROM m_user");
-        //console.log(users.rows);
-        res.json(authenticatedUser)
-        //res.json(users.rows);
+        const authenticatedUser = (req as any).user_id;
+        
+        const existingLike = await pool.query(
+            "SELECT * FROM m_like WHERE user_id = $1 AND painting_id = $2",
+            [authenticatedUser, req.params.painting_id]
+        );
+
+        if (existingLike.rows.length > 0) {
+            return res.status(400).json({
+                error_message: "You've already liked this painting.",
+            });
+        }
+
+        await pool.query(
+            "INSERT INTO m_like(user_id, painting_id, created_at) VALUES ($1, $2, $3)",
+            [authenticatedUser, req.params.painting_id, new Date()]
+        );
+
+        res.json({ message: "Painting liked successfully." });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "An error occurred." });
+        res.status(400).json({ error: "An error occurred." });
     }
 });
 
